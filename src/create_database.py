702ec3960d41f5b5202a73d31681a581a688fccf
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-04-06 22:36:52 +0200

    feat:add connection to supabase database

--- src/create_database.py
+++ src/create_database.py
@@ -1,10 +1,8 @@
-import os
-import shutil
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader, UnstructuredMarkdownLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
-from config import CHROMA_DB_PATH, EMBEDDING_MODEL, DATA_PATH
+import vecs
+from config import EMBEDDING_MODEL, DATA_PATH, SUPABASE_DB_URL
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -12,9 +10,13 @@ logger = logging.getLogger(__name__)
 
 
 class RAGDatabase:
-    def __init__(self, data_path=DATA_PATH, db_path=CHROMA_DB_PATH, embedding_model=EMBEDDING_MODEL,format="pdf"):
+    def __init__(self, data_path=DATA_PATH, embedding_model=EMBEDDING_MODEL,format="pdf"):
         self.data_path = data_path
-        self.db_path = db_path
+        self.vx=vecs.create_client(SUPABASE_DB_URL)
+        self.collection=self.vx.get_or_create_collection(
+            name="rag_collection",
+            dimension=768
+        )
         self.embedding_model = OllamaEmbeddings(model=embedding_model)
         self.format = format
         self.loaders={
@@ -27,6 +29,7 @@ class RAGDatabase:
         docs=self._load_document()
         chunks=self._split_documents(docs)
         self._save_to_db(chunks)
+        self.vx.disconnect()
 
     def _load_document(self):
         loader = DirectoryLoader(self.data_path, glob=f"*.{self.format}", loader_cls=self.loaders.get(self.format))
@@ -45,12 +48,26 @@ class RAGDatabase:
         return chunks
     
     def _save_to_db(self, chunks):
-        if os.path.exists(self.db_path):
-            shutil.rmtree(self.db_path)
-
-        db=Chroma.from_documents(chunks,embedding=self.embedding_model,persist_directory=self.db_path)
-        logger.info(f"Saved {len(chunks)} chunks to ChromaDB at {self.db_path}.")
-
-
+        records=[]
+        for i, chunk in enumerate(chunks):
+            embedding = self.embedding_model.embed_query(chunk.page_content)
+            records.append((
+                f"chunk_{i}",      
+                embedding,          
+                {                   
+                    "content":  chunk.page_content,
+                    "source":   chunk.metadata.get("source", "unknown"),
+                    "page":     chunk.metadata.get("page", 0),
+                }
+            ))
+        batch_size=100
+        for i in range(0, len(records), batch_size):
+            batch=records[i:i+batch_size]
+            self.collection.upsert(records=batch)
+            logger.info(f"Uploaded {min(i + batch_size, len(records))}/{len(chunks)} chunks")
+        
+        logger.info(f"Saved {len(chunks)} chunks to the database.")
+        self.collection.create_index()
+    
 if __name__ == "__main__":
     RAGDatabase().build()
