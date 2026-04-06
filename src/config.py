91e3b98352f08cc17dde3a857122456c1182b58d
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-04-06 22:35:05 +0200

    chore:add configuration file

--- src/config.py
+++ src/config.py
@@ -1,9 +1,15 @@
 # config.py
 import os
+import dotenv
+from dotenv import load_dotenv
 
+load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, "data")
-CHROMA_DB_PATH = os.path.join(BASE_DIR, "db", "chroma_db")
 
 EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
 LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+RERANKING_MODEL = "ms-marco-MiniLM-L-12-v2"
+
+
+SUPABASE_DB_URL = os.getenv("SUPABASE_DB_URL")
