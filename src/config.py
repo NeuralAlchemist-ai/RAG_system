bd37ea469d29e4e2d578aeed1d0d8403729c7243
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-04-08 12:10:35 +0200

    chore: update LANGUAGE_MODEL and RERANKING_MODEL to faster models

--- src/config.py
+++ src/config.py
@@ -7,9 +7,8 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, "data")
 
-EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
-LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
-RERANKING_MODEL = "ms-marco-MiniLM-L-12-v2"
-
+EMBEDDING_MODEL = "nomic-embed-text"
+LANGUAGE_MODEL =  "llama3.2:3b" 
+RERANKING_MODEL = "ms-marco-MiniLM-L-6-v2"
 
 SUPABASE_DB_URL = os.getenv("SUPABASE_DB_URL")
